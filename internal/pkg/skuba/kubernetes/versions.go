9d7e727848dc60d361e7e24785aed8351558dc40
Michal Rostecki <mrostecki@suse.de>
2020-03-10 15:22:12 +0100

    versions: Update Cilium to 1.6.6
    Update Cilium image tag to 1.6.6. That new image contains also Envoy in
    version 1.12.2.
    
    Signed-off-by: Michal Rostecki <mrostecki@suse.de>

--- internal/pkg/skuba/kubernetes/versions.go
+++ internal/pkg/skuba/kubernetes/versions.go
@@ -94,7 +94,7 @@ var (
 				Tooling:   &ContainerImageTag{Name: "skuba-tooling", Tag: "0.1.0"},
 			},
 			AddonsVersion: AddonsVersion{
-				Cilium:        &AddonVersion{"1.5.3", 2},
+				Cilium:        &AddonVersion{"1.6.6", 3},
 				Kured:         &AddonVersion{"1.3.0", 4},
 				Dex:           &AddonVersion{"2.16.0", 5},
 				Gangway:       &AddonVersion{"3.1.0-rev4", 4},
